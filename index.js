3cadadb8c703500702b1fd4b12e55674b275f0ee
Jaclyn Setina <jaclyn.setina@gmail.com>
2019-01-11 01:44:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   if( katzDeliLine.length ===0) {
-    return("The line is currently empty")
+    return("The line is currently empty.")
   }
   else {
     return("The line is currently:" + katzDeliLine[0] + ", " + katzDeliLine[1])
